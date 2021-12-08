d7d11fcf5b173c8220bee3e87792f544e2ba1d45
Jakub Chmielewski <j.chmielewski1008@gmail.com>
2021-12-08 18:45:38 +0100

    adding word randomizer in HangmanHelper class

--- src/pl/chmielewski/hangmanGame/HangmanGame.java
+++ src/pl/chmielewski/hangmanGame/HangmanGame.java
@@ -4,14 +4,16 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class HangmanGame {
-
+    private HangmanHelper hangmanHelper=new HangmanHelper();
     public static void main(String[] args) {
 
 
         HangmanGame hangmanGame=new HangmanGame();
         Hangman hangman=new Hangman();
+
         String[] alphabetArray={"a","b","c","d","e","f","g","h","i","j","k","l","ł","m","n","o","ó","p","r","s","ś","t","u","w","y","z","ź","ż"};
         HashSet<String> alphabet=new HashSet<>(Arrays.asList(alphabetArray));
+        HashSet<String> usedLetters=new HashSet<>(alphabetArray.length);
         hangmanGame.startGame();
         //hangmanMessagePrinter.printGameMenu();
     }
@@ -20,5 +22,6 @@ public class HangmanGame {
          HangmanMessagePrinter hangmanMessagePrinter=new HangmanMessagePrinter();
          hangmanMessagePrinter.printStartingMessage();
          hangmanMessagePrinter.printGameInstructions();
+         hangmanHelper.randomizeWord();
     }
 }
--- src/pl/chmielewski/hangmanGame/HangmanHelper.java
+++ src/pl/chmielewski/hangmanGame/HangmanHelper.java
@@ -1,4 +1,18 @@
 package pl.chmielewski.hangmanGame;
 
+import java.util.HashSet;
+import java.util.Random;
+
 public class HangmanHelper {
+
+    String[] wordsArray={"lekkoatletyka","interpunkcja","telekomunikacja","metamorfoza","zwierzchnictwo"," prześladowanie","antyterrorysta","dźwiękonaśladownictwo","antykoncepcja","kolorowanka","konstantynopolitański","lumpenproletariat","luminescencja","magnetoelektryczny","malkontenctwo","primaaprilisowy","pięćdziesięciogroszówka","anatomopatologiczny","deoksyrybonukleinowy",};
+    HashSet<String> words=new HashSet<>(20);
+
+    public void randomizeWord() {
+        Random rand = new Random();
+
+    String randomizedWord=wordsArray[rand.nextInt(25)];
+        System.out.println(randomizedWord);
+
+    }
 }
